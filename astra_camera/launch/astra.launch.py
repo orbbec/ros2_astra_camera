64e921687362dfe0e884f989ed5a0ddada2ef506
Joe Dong <jian.dong@pobox.com>
2022-06-27 15:51:31 +0800

    add parameter files

---
+++ astra_camera/launch/astra.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python import get_package_share_directory
+
+
+def generate_launch_description():
+    astra_params_file = (get_package_share_directory("astra_camera") +
+                         "/params/astra_params.yaml")
+    return LaunchDescription([
+        Node(
+            package="astra_camera",
+            namespace="camera",
+            name="camera",
+            executable="astra_camera_node",
+            output="screen",
+            parameters=[astra_params_file],
+        ),
+    ])
