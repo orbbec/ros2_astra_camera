0669670e1b0d6ff2b18d7c55eb5b9a2855370ff1
Joe Dong <jian.dong@pobox.com>
2022-08-02 10:36:13 +0800

    fix launch file error

--- astra_camera/launch/dabai_dw.launch.py
+++ astra_camera/launch/dabai_dw.launch.py
@@ -22,12 +22,18 @@ def generate_launch_description():
                 name='camera',
                 namespace='camera',
                 parameters=[config_params]
-            ),
+                ),
             ComposableNode(
                 package='astra_camera',
                 plugin='astra_camera::PointCloudXyzNode',
                 namespace='camera',
-                name='point_cloud_xyz')
+                name='point_cloud_xyz'),
+            ComposableNode(
+                package='astra_camera',
+                plugin='astra_camera::PointCloudXyzrgbNode',
+                namespace='camera',
+                name='point_cloud_xyzrgb')
+        ],
         output='screen'
     )
     return LaunchDescription([container])
