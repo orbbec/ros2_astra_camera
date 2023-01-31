ea2d65a976f5f2cd72083c2c026ac240b6f2de1a
Joe Dong <jian.dong@pobox.com>
2023-01-31 10:40:22 +0800

    fixed mising rclcpp::shutdown()

--- astra_camera/src/main.cpp
+++ astra_camera/src/main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char** argv) {
   rclcpp::NodeOptions options;
   auto node = std::make_shared<astra_camera::OBCameraNodeFactory>(options);
   rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
